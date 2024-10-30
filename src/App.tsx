d8b2ae6717e2efb65e018ce245b3d820d21046d3
maxinth <tunde.j.adeleye@gmail.com>
2024-10-30 23:15:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="bg-white h-screen flex items-center justify-center">
+      hello
+    </main>
+  );
+}
+
+export default App;
