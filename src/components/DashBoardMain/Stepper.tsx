26675ed1ae1d5760219d923ee7cbecbdb28c04e5
maxinth <tunde.j.adeleye@gmail.com>
2024-11-01 13:55:59 +0100

    ..

--- src/components/DashBoardMain/Stepper.tsx
+++ src/components/DashBoardMain/Stepper.tsx
@@ -5,11 +5,11 @@ export const Stepper = () => {
   return (
     <Box className="!text-img-black px-4 hidden lg:flex items-center justify-between font-bold text-4xl  -top-4 relative mb-4 z-100 ">
       <StepItem Icon={<CompletedStepIcon />} text="Eligibility Agent" />
-      <hr className="border-2 border-[#F3F3F3] w-1/6 relative -top-4" />
+      <hr className="border-2 border-[#F3F3F3] w-1/6 relative -top-6" />
       <StepItem Icon={<CurrentStepIcon />} text="Document Agent" />{" "}
-      <hr className="border-2 border-[#F3F3F3] w-1/6 relative -top-4" />
+      <hr className="border-2 border-[#F3F3F3] w-1/6 relative -top-6" />
       <StepItem Icon={<CurrentStepIcon />} text="Form-Filling Agent" />{" "}
-      <hr className="border-2 border-[#F3F3F3] w-1/6 relative -top-4" />
+      <hr className="border-2 border-[#F3F3F3] w-1/6 relative -top-6" />
       <StepItem Icon={<CurrentStepIcon />} text="Interview Prep Agent" />
     </Box>
   );
